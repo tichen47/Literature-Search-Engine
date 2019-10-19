91a388c3b534822bf0934a6ac1eee04036ddf65b
Ti Chen <tichen47@gmail.com>
2019-10-19 00:19:25 -0500

    serverless version

--- search.py
+++ search.py
@@ -1,4 +1,14 @@
-# indexing
+import sys
 
 
-#query index
+
+if __name__ == '__main__':
+    print(sys.argv[1])
+
+    dict = {
+        "": "Ford",
+        "model": "Mustang",
+        "year": 1964
+    }
+    
+
